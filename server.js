06aefd2f53d26109e7a604918d190bbaa6b1131f
Sanjeev Kushwaha <SanjeevKumarp339@gmail.com>
2026-03-21 18:40:01 +0530

    update server config

--- server.js
+++ server.js
@@ -33,11 +33,13 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
+app.set('trust proxy', 1);
 
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 300
 }));
+
 app.use('/api/', rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 300
