3b5e62ad99d9f14d17c21ed38d064ee7ca325bd9
Sanjeev Kushwaha <SanjeevKumarp339@gmail.com>
2026-03-23 01:17:33 +0530

    config: default Firebase projectId to localskill-83095

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  const projectId = process.env.FIREBASE_PROJECT_ID;
+  const projectId = process.env.FIREBASE_PROJECT_ID || 'localskill-83095';
   const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
   const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
